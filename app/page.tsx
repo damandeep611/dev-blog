ded989c88dabb4a58d11a73421f419bb5d9976a9
devDaman <damandeep611@gmail.com>
2024-09-30 15:37:47 +0530

    error resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
-import Link from "next/link";
-import { draftMode } from "next/headers";
-
-import Date from "./date";
-import CoverImage from "./cover-image";
-import Avatar from "./avatar";
-import MoreStories from "./more-stories";
-
-import { getAllPosts } from "@/lib/api";
 import Hero from "./components/ui/Hero";
 import Banner from "./components/ui/Banner";
 
-
 export default async function Page() {
-  const { isEnabled } = draftMode();
-  const allPosts = await getAllPosts(isEnabled);
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
-
   return (
     <div className="container mx-auto px-2 lg:px-5 flex flex-col items-center ">
       <Hero />
